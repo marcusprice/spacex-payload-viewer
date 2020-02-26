dd99c363a599c19fb4d583af6476217b62573edc
Price <marcusprice88@gmail.com>
2020-02-26 11:28:46 -0800

    got header layout working

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Header = () => {
+  return(
+    <header>
+      <h1>Spacex Payload Viewer</h1>
+      <nav>
+        <ul>
+          <li>Payload Data</li>
+          <li>Rocket Data</li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export  default Header;
