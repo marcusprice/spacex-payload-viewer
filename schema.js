fcac5e66f205a3e0760520f3d1a61a03016bf344
Marcus Price <marcusprice88@gmail.com>
2020-02-26 09:57:50 -0800

    set up initial schema & verified that api works

---
+++ schema.js
@@ -0,0 +1,40 @@
+const axios = require('axios')
+
+const {
+  GraphQLObjectType,
+  GraphQLInt,
+  GraphQLString,
+  GraphQLList,
+  GraphQLSchema
+} = require('graphql')
+
+//payload type
+const PayloadType = new GraphQLObjectType({
+  name: 'payload',
+  fields: () => ({
+    payload_id: { type: GraphQLString },
+    nationality:  { type: GraphQLString },
+    manufacturer:  { type: GraphQLString },
+    payload_type:  { type: GraphQLString },
+    payload_mass_lbs: { type: GraphQLString }
+  })
+})
+
+//root query
+const RootQuery = new GraphQLObjectType({
+  name: 'RootQueryType',
+  fields: {
+    payloads: {
+      type: new GraphQLList(PayloadType),
+      resolve(parent, args) {
+        //get data from spacex api
+        return axios.get('https://api.spacexdata.com/v3/payloads')
+          .then(res => res.data)
+      }
+    }
+  }
+})
+
+module.exports = new GraphQLSchema({
+  query: RootQuery
+})
