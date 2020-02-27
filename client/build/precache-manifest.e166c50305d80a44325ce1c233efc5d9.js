2e9b1a9d575c9d569f0e668e2be3f54ddef01ef2
Marcus Price <marcusprice88@gmail.com>
2020-02-26 22:20:07 -0800

    removed build folder from gitignore

---
+++ client/build/precache-manifest.e166c50305d80a44325ce1c233efc5d9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "728bfabbd1a4e35aa396e93d3a2c1630",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9fac04cebf385f684363",
+    "url": "/static/css/main.7c6c2999.chunk.css"
+  },
+  {
+    "revision": "5028417019c9e031c93e",
+    "url": "/static/js/2.1cab0e08.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.1cab0e08.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9fac04cebf385f684363",
+    "url": "/static/js/main.8d937652.chunk.js"
+  },
+  {
+    "revision": "cda345fb50b7e11c0025",
+    "url": "/static/js/runtime-main.844668cf.js"
+  },
+  {
+    "revision": "60d82d74a2d0662a8a372901a63b9123",
+    "url": "/static/media/payload.60d82d74.jpg"
+  },
+  {
+    "revision": "3e2d58879790e72d28b07e4ddfd697cb",
+    "url": "/static/media/rocket.3e2d5887.jpg"
+  }
+]);
